3d0d0e19fa60cd645508e7aace53884da355dfb0
ertegun <ertegun@users.noreply.github.com>
2020-11-01 22:18:01 +0300

    home page
    home page, images, icons

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,20 +1,22 @@
-<nav>
+<nav class="purple darken-4">
     <div class="nav-wrapper">
-        <a href="#" class="brand-logo">Logo</a>
+        <a href="#" class="brand-logo">
+            <img style="margin: 0px 0px 0 5px;" width="50" src="/assets/images/crow.png" alt="" srcset="">
+        </a>
         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 
         <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/projects">Projects</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <!-- <li><a href="/about">About</a></li> -->
+            <!-- <li><a href="/projects">Projects</a></li> -->
+            <!-- <li><a href="/contact">Contact</a></li> -->
         </ul>
     </div>
 </nav>
 
 <ul class="sidenav" id="mobile-demo">
     <li><a href="/">Home</a></li>
-    <li><a href="/about">About</a></li>
-    <li><a href="/projects">Projects</a></li>
-    <li><a href="/contact">Contact</a></li>
+    <!-- <li><a href="/about">About</a></li> -->
+    <!-- <li><a href="/projects">Projects</a></li> -->
+    <!-- <li><a href="/contact">Contact</a></li> -->
 </ul>
