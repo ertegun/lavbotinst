d932de793f422f7c0b97730ea58cae2410b20e39
ertegun <ertegun@users.noreply.github.com>
2020-11-01 22:31:23 +0300

    og title remove
    og title remove

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,7 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta property="og:type" content="article">
 <meta property="og:description" content="{{$messages[0]->media_share['caption']['text']}}">
-<meta property="og:title" content="{{$messages[0]->media_share['caption']['text']}}">
+<!-- <meta property="og:title" content="{{$messages[0]->media_share['caption']['text']}}"> -->
 
 <meta property="og:image" content="{{$messages[0]->media_share['image_versions2']['candidates'][0]['url']}}">
 <link rel="image_src" href="{{$messages[0]->media_share['image_versions2']['candidates'][0]['url']}}">
